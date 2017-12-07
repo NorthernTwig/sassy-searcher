139e39425273c2c8a679fefa4c7718e1a249d123
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-07 11:12:28 +0100

    Controls data flow

--- src/main/java/sassySearcher/Entry.java
+++ src/main/java/sassySearcher/Entry.java
@@ -1,12 +1,34 @@
 package sassySearcher;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class Entry {
     @RequestMapping("/")
-    public String entry() {
-        return "hello!";
+    public String entry(@RequestParam(value = "query", defaultValue = "") String param) throws IOException {
+        String lowParam = param.toLowerCase();
+
+        FileHandler handler = new FileHandler();
+        List<Path> whiskeyPaths = handler.readAll("Whiskey");
+        List<Path> woodworkingPaths = handler.readAll("Woodworking");
+        List<Path> paths = new ArrayList<>();
+        paths.addAll(whiskeyPaths);
+        paths.addAll(woodworkingPaths);
+
+        PageDB db = new PageDB();
+
+        Page page = new Page(db);
+        page.create(paths);
+
+        Query query = new Query(db);
+        ArrayList<SearchResult> result = query.query(lowParam);
+
+        Presenter presenter = new Presenter(result, lowParam);
+        return presenter.getResult();
     }
 }
