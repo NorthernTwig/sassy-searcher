c457f8384ca399396885af1f09bd70234e331ef7
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-07 11:10:16 +0100

    Presents search results

---
+++ src/main/java/sassySearcher/Presenter.java
@@ -0,0 +1,32 @@
+package sassySearcher;
+
+import java.util.ArrayList;
+
+public class Presenter {
+    String query;
+    ArrayList<SearchResult> result;
+    int amount = 5;
+
+    public Presenter(ArrayList<SearchResult> result, String query) {
+        this.query = query;
+        this.result = result;
+    }
+
+    public String getResult() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("<h3>Result for search query: ").append(query).append("</h3>");
+        builder.append("<ul>");
+        for (int i = 0; i < amount; i++) {
+            builder.append("<li>").append(getResult(i)).append("</li>");
+        }
+        builder.append("</ul>");
+
+        return builder.toString();
+    }
+
+    private String getResult(int i) {
+        return result.get(i).toString();
+    }
+
+}
