b8cb9e4cce0be80d54945e68f4f67eb126f4c5c7
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-08 10:27:05 +0100

    Added pagerank functionallity

--- src/main/java/sassySearcher/models/Page.java
+++ src/main/java/sassySearcher/models/Page.java
@@ -8,17 +8,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Page implements Serializable {
-    String url;
+    public String url;
     public ArrayList<Integer> words;
-    PageDB db;
+    public PageDB db;
+    public double pageRank = 1.0;
+    public ArrayList<String> links;
+    private int ITERATIONS = 20;
 
     public Page(PageDB db) {
         this.db = db;
+        this.links = new ArrayList<>();
     }
+
     public Page(String url, ArrayList<Integer> words, PageDB db) {
         this.url = url;
         this.words = words;
         this.db = db;
+        this.links = new ArrayList<>();
     }
 
     public void create(List<Path> paths) throws IOException {
@@ -32,7 +38,7 @@ public class Page implements Serializable {
             ArrayList<Integer> wordCollection = new ArrayList<>();
 
             Files.lines(wordsFile).map(line -> line.split(" ")).forEach(words -> {
-                for (String word: words) {
+                for (String word : words) {
                     int id = db.getWordId(word);
                     wordCollection.add(id);
                 }
@@ -40,8 +46,41 @@ public class Page implements Serializable {
 
             Page page = new Page(url, wordCollection, db);
             db.addPage(page);
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
+            calculatePageRank();
+        } catch (Exception e) {
+            System.out.println(e);
         }
+    }
+
+    public void calculatePageRank() {
+        for (int i = 0; i < ITERATIONS; i++) {
+            for (Page page : db.getPages()) {
+                iteratePageBank(page);
+            }
+        }
+    }
+
+    public boolean hasLinkTo(String pageUrl) {
+        return links.contains(pageUrl);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getNoLinks() {
+        return links.size();
+    }
+
+    private void iteratePageBank(Page page) {
+        double pr = 0;
+        for (Page pa : db.getPages()) {
+            if (pa.hasLinkTo(page.getUrl())) {
+                pr += pa.pageRank / (double) pa.getNoLinks();
+            }
+        }
+
+        pr = 0.85 * pr + 0.15;
+        page.pageRank = pr;
     }
 }
