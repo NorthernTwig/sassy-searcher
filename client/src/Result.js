0a89796b6f7a86212cfab6bbf4e5b456020b54c4
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-08 12:29:14 +0100

    Properly display results

--- client/src/Result.js
+++ client/src/Result.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 
 export default class Result extends Component {
   render() {
-    return this.props.result.map(data => (
-      <div key={data.name}>
-        <h1>{data.name}</h1>
-        <h5>{data.score}</h5>
+    return (
+      <div className="result-container">
+        {this.props.result.map(data => (
+          <div key={data.name}>
+            <h3>{data.name}</h3>
+            <h5>{data.score}</h5>
+          </div>
+        ))}
       </div>
-    ))
+    )
+    
   }
 }
