bdafd93c16f7967f4a912c5d4160b6343ed880cf
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-08 11:05:45 +0100

    Listeners init

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react'
+import Result from './Result'
 
 class App extends Component {
+  state = { query: '', result: [] }
+
+  setQuery = ({ target: { value: query } }) => this.setState({ query })
+
+  search = () => {
+    const result = [{ name: 'Islay', score: 1.5 }, { name: 'Denmark', score: 1.0 }]
+    this.setState({ result })
+  }
+
   render() {
-    return <h1>Sloogle</h1>
+    return (
+      <div>
+        <h1>Sloogle</h1>
+        <input type="text" onChange={this.setQuery} />
+        <button onClick={this.search}>search</button>
+        <Result result={this.state.result} />
+      </div>
+    )
   }
 }
 
