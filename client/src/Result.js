6eadac30698a8cb538bb759403d81c12f291f96c
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-08 13:21:14 +0100

    Removed underscore from displayed name

--- client/src/Result.js
+++ client/src/Result.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 
 export default class Result extends Component {
+  removeUnderscore = name => {
+    return name.replace(/_/g, " ")
+  }
+
+
   render() {
     return (
       <div className="result-container">
         {this.props.result.map(data => (
           <div key={data.name}>
             <a href={`https://en.wikipedia.org/wiki/${data.name}`} target="_blank">
-              <h3>{data.name}</h3>
+              <h3>{this.removeUnderscore(data.name)}</h3>
             </a>
             <h5>{data.score}</h5>
           </div>
